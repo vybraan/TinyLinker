3d39f4f2fcbb24b6a1ba01a01709ab9b80d6b78b
Ailton Baúque <ailtonbauque@outlook.com>
2024-10-18 06:04:42 +0200

    Fix issue with cached shortcode not incrementing click stats

--- tldash/src/lib/redisClient.ts
+++ tldash/src/lib/redisClient.ts
@@ -2,14 +2,17 @@ import { createClient } from 'redis';
 
 const redisClient = createClient({
   url: process.env.REDIS_URL || 'redis://127.0.0.1:6379',
+  socket: {
+    reconnectStrategy: (retries) => {
+      console.log(`Reconnecting attempt #${retries}...`);
+      return 700; // 0.7 seconds
+    }
+  }
 });
 
 redisClient.on('error', (err) => {
-  // console.error('Redis Client Error:', err);
-  console.error('Redis Client Error: ');
-  throw Error(err.message);
-
-
+  console.error('Redis Client Error:', err.message);
+  redisClient.quit(); 
 });
 
 redisClient.on('connect', () => {
@@ -23,5 +26,17 @@ redisClient.on('ready', () => {
 redisClient.on('reconnecting', () => {
   console.log('Reconnecting to Redis...');
 });
+
+redisClient.on('end', () => {
+  console.log('Redis connection closed.');
+});
+
+const cleanup = () => {
+  console.log('Cleaning up Redis connection...');
+  redisClient.quit();
+};
+
+process.on('SIGINT', cleanup);
+process.on('SIGTERM', cleanup);
 
 export default redisClient;
